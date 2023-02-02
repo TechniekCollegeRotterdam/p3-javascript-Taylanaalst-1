fb9350d567dc07dfd267b3b5153b17cc38a7211d
Taylanaalst <90317611+Taylanaalst@users.noreply.github.com>
2023-02-02 13:51:28 +0100

    Opdracht 2.2/2.3/2.4 updated

--- Opdracht2.4/app.js
+++ Opdracht2.4/app.js
@@ -1,6 +1,25 @@
+//1
+function calc(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+}
+
+calc(10, 15)
+
+
+//2
 function multiplyByFive(num) {
     let answer = 5 * num;
     console.log(answer);
 }
 
 multiplyByFive(2)
+
+//3
+
+function calculateMinutes(num) {
+    let answer = num / 60;
+    console.log(answer);
+}
+
+calculateMinutes(3600)
