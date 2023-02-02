6cd688eeae7c93d9d8fd7a7731f00c3668361aef
Taylanaalst <90317611+Taylanaalst@users.noreply.github.com>
2023-02-02 11:16:32 +0100

    Opdracht2.4

---
+++ Opdracht2.4/app.js
@@ -0,0 +1,6 @@
+function calc(num, num2) {
+    let answer = num * num2;
+    console.log(answer);
+}
+
+calc(10, 2)
--- test/app.js
+++ test/app.js
@@ -12,7 +12,7 @@
 //   console.log(`good day ${time} ${name}`);
 // };
 
-const calcArea = function(radius, name='', titel='') {
+const calcArea = function(radius) {
     let area = 3.14 * radius**2;
     console.log(area);
 }
