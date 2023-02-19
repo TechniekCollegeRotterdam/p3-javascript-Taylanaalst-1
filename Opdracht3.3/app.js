8b1597f2bb73688ac0052fa44e8d5fc9b528bac7
Jason <83275145+jschaik@users.noreply.github.com>
2023-02-19 15:02:38 +0100

    feedback

--- Opdracht3.3/app.js
+++ Opdracht3.3/app.js
@@ -2,11 +2,12 @@ let clicks = 0;
 
 function onClick() {
   clicks += 1;
-  document.getElementById("clicks").innerText = clicks;
+  document.getElementById("clicks").innerText = clicks; //niet in de functie maken maar buiten de functie en dus opslaan in een variabele
 
   if (clicks == 10) {
     clicks -= 10;
-};
+    //hier moet clicks weer bij 0 beginnen, oftewel: clicks = 0;
+}; //bij een if moet ook een else, wat moet daar komen te staan?
 
 };
 
