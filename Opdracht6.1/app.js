df95cff7eaec70b86398f300c53b15d75ec9603e
Taylanaalst <90317611+Taylanaalst@users.noreply.github.com>
2023-03-16 10:31:00 +0100

    Opdracht 6.1

---
+++ Opdracht6.1/app.js
@@ -0,0 +1,15 @@
+let text = document.getElementById("inputField");
+let button = document.getElementById("addToDo");
+let container = document.getElementById("toDoContainer");
+
+button.addEventListener("click", function() {
+    let p = document.createElement("p");
+    p.innerText = text.value;
+    container.appendChild(p)
+    
+
+    text.value = "";
+
+    
+    
+})
