5e3e3862d2464e248255e74f5440312f4d151779
Taylanaalst <90317611+Taylanaalst@users.noreply.github.com>
2023-04-11 13:44:55 +0200

    Opdracht countdown

---
+++ Opdracht7.1/app.js
@@ -0,0 +1,9 @@
+let product1 = "Bananen";
+let product2 = "Whiskey";
+let product3 = "Chips";
+let product4 = "Bonen";
+let product5 = "Aardappelen";
+
+let winkelmand = ["Bananen","Whiskey","Chips", "Bonen", "Aardappelen"]
+
+document.getElementById("demo").innerText = winkelmand;
